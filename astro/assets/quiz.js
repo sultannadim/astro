ddf3d8e29a99b5b6b7c01cc8ee3d0e1e25e999b2
sultan5218 <sultan5218@gmail.com>
2022-07-16 10:06:47 +0600

    first commit

---
+++ astro/assets/quiz.js
@@ -0,0 +1,65 @@
+$('#quiz').quiz({
+    counterFormat: 'Question %current of %total',
+    questions: [
+      {
+        'q': 'Question? One',
+        'options': [
+          'Answer 1',
+          'Answer 2',
+          'Answer 3',
+          'Answer 4'
+        ],
+        'correctIndex': 1,
+        //'correctResponse': 'Custom correct response.',
+        //'incorrectResponse': 'Custom incorrect response.'
+      },
+      {
+        'q': 'Question? Two ',
+        'options': [
+          'Answer 1',
+          'Answer 2',
+          'Answer 3',
+          'Answer 4'
+        ],
+        'correctIndex': 3,
+        //'correctResponse': 'Custom correct response.',
+        //'incorrectResponse': 'Custom incorrect response.'
+      },
+      {
+        'q': 'Question? Three ',
+        'options': [
+          'Answer 1',
+          'Answer 2',
+          'Answer 3',
+          'Answer 4'
+        ],
+        'correctIndex': 3,
+        //'correctResponse': 'Custom correct response.',
+        //'incorrectResponse': 'Custom incorrect response.'
+      },
+      {
+        'q': 'Question? Four ',
+        'options': [
+          'Answer 1',
+          'Answer 2',
+          'Answer 3',
+          'Answer 4'
+        ],
+        'correctIndex': 3,
+        //'correctResponse': 'Custom correct response.',
+        //'incorrectResponse': 'Custom incorrect response.'
+      },
+      {
+        'q': 'Question? Five ',
+        'options': [
+          'Answer 1',
+          'Answer 2',
+          'Answer 3',
+          'Answer 4'
+        ],
+        'correctIndex': 3,
+        //'correctResponse': 'Custom correct response.',
+        //'incorrectResponse': 'Custom incorrect response.'
+      }
+    ]
+  });
