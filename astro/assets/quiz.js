6633c8249fe2d85c478c3fa3dccced452451ab6f
sultan5218 <sultan5218@gmail.com>
2022-07-19 17:19:44 +0600

    all

--- astro/assets/quiz.js
+++ astro/assets/quiz.js
@@ -1,65 +1,40 @@
-$('#quiz').quiz({
-    counterFormat: 'Question %current of %total',
-    questions: [
-      {
-        'q': 'Question? One',
-        'options': [
-          'Answer 1',
-          'Answer 2',
-          'Answer 3',
-          'Answer 4'
-        ],
-        'correctIndex': 1,
-        //'correctResponse': 'Custom correct response.',
-        //'incorrectResponse': 'Custom incorrect response.'
-      },
-      {
-        'q': 'Question? Two ',
-        'options': [
-          'Answer 1',
-          'Answer 2',
-          'Answer 3',
-          'Answer 4'
-        ],
-        'correctIndex': 3,
-        //'correctResponse': 'Custom correct response.',
-        //'incorrectResponse': 'Custom incorrect response.'
-      },
-      {
-        'q': 'Question? Three ',
-        'options': [
-          'Answer 1',
-          'Answer 2',
-          'Answer 3',
-          'Answer 4'
-        ],
-        'correctIndex': 3,
-        //'correctResponse': 'Custom correct response.',
-        //'incorrectResponse': 'Custom incorrect response.'
-      },
-      {
-        'q': 'Question? Four ',
-        'options': [
-          'Answer 1',
-          'Answer 2',
-          'Answer 3',
-          'Answer 4'
-        ],
-        'correctIndex': 3,
-        //'correctResponse': 'Custom correct response.',
-        //'incorrectResponse': 'Custom incorrect response.'
-      },
-      {
-        'q': 'Question? Five ',
-        'options': [
-          'Answer 1',
-          'Answer 2',
-          'Answer 3',
-          'Answer 4'
-        ],
-        'correctIndex': 3,
-        //'correctResponse': 'Custom correct response.',
-        //'incorrectResponse': 'Custom incorrect response.'
-      }
-    ]
-  });
+$("#quiz").quiz({
+  counterFormat: "Question %current of %total",
+  questions: [
+    {
+      q: "Question? One",
+      options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+      correctIndex: 1,
+      //'correctResponse': 'Custom correct response.',
+      //'incorrectResponse': 'Custom incorrect response.'
+    },
+    {
+      q: "Question? Two ",
+      options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+      correctIndex: 3,
+      //'correctResponse': 'Custom correct response.',
+      //'incorrectResponse': 'Custom incorrect response.'
+    },
+    {
+      q: "Question? Three ",
+      options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+      correctIndex: 3,
+      //'correctResponse': 'Custom correct response.',
+      //'incorrectResponse': 'Custom incorrect response.'
+    },
+    {
+      q: "Question? Four ",
+      options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+      correctIndex: 3,
+      //'correctResponse': 'Custom correct response.',
+      //'incorrectResponse': 'Custom incorrect response.'
+    },
+    {
+      q: "Question? Five ",
+      options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+      correctIndex: 3,
+      //'correctResponse': 'Custom correct response.',
+      //'incorrectResponse': 'Custom incorrect response.'
+    },
+  ],
+});
